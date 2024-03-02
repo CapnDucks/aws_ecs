15ae93079a18a647b2510ac7c4f5ef2f7089cd55
Cap'n Duck <coder@capnduck.com>
2024-03-01 16:47:21 -0800

    Updating example.

--- example/main.tf
+++ example/main.tf
@@ -1,10 +1,7 @@
 module "ecs" {
   source = "../"
 
-  kms_key_alias      = "KMS-ALIAS"
-  name               = "ECS-NAME"
-  cluster_name       = "CLUSTER-NAME"
-  container_insights = true
+  name = "My ECS Cluster"
 
   tags = {
     Name = "ECS module example"
