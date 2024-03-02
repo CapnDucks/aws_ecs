82935e9cf301218e78701e506b98fc57844d25be
Cap'n Duck <coder@capnduck.com>
2024-03-01 16:41:33 -0800

    Updating outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,10 @@
-#output "ecs_cluster_id" {
-#  description = "ID of the ECS Cluster"
-#  value       = concat(aws_ecs_cluster.this.*.id, [""])[0]
-#}
-
-output "ecs_cluster_id" {
-  description = "ID of the ECS Cluster."
-  value       = [for instances in aws_ecs_cluster.this : instances.id]
-}
-
 output "ecs_cluster_arn" {
-  description = "ARN of the ECS Cluster."
-  value       = concat(aws_ecs_cluster.this.*.arn, [""])[0]
+  description = "ARN of the ECS Cluster"
+  #value      = concat(aws_ecs_cluster.this.*.arn, [""])[0]
+  value       = aws_ecs_cluster.this.arn
 }
 
 output "ecs_cluster_name" {
-  description = "The name of the ECS cluster."
+  description = "The name of the ECS cluster"
   value       = var.name
 }
-
-#output "kms_key_arn" {
-#  value = module.kms.kms_key["arn"]
-#}
